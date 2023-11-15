a51c928d57c7c72253d4afe526de95de7706aa2a
George Murphy <321ebikes@gmail.com>
2023-11-15 16:40:01 -0500

    added sliders to adjust paramerters

--- Task-Completion-Simulation.py
+++ Task-Completion-Simulation.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 
 # Function to simulate task completion rates
-def simulate_completion_rate(num_steps, dt, a, b, c):
+def simulate_completion_rate(num_steps, dt, a, b, c, noise_type):
     # Initialize arrays to store time and completion rate
     time = np.zeros(num_steps)
     completion_rate = np.zeros(num_steps)
@@ -13,7 +14,7 @@ def simulate_completion_rate(num_steps, dt, a, b, c):
 
     # Generate Wiener process (Brownian motion)
     
-    noise_type = 'brownian'
+    
     if noise_type == 'normal':
         dW = np.random.normal(0, np.sqrt(dt), 1)
     elif noise_type == 'brownian':
@@ -47,12 +48,37 @@ a = 0.1
 b = 0.02
 c = 0.1
 
-# Run the simulation
-time, completion_rate = simulate_completion_rate(num_steps, dt, a, b, c)
+# Create figure and axes
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.25)  # Adjust the bottom to make room for sliders
 
-# Plot the results
-plt.plot(time, completion_rate)
-plt.title('Task Completion Rate Over Time')
-plt.xlabel('Time')
-plt.ylabel('Completion Rate')
+# Plot the initial results
+time, completion_rate = simulate_completion_rate(num_steps, dt, a, b, c, noise_type="brownian")
+line, = ax.plot(time, completion_rate)
+ax.set_title('Task Completion Rate Over Time')
+ax.set_xlabel('Time')
+ax.set_ylabel('Completion Rate')
+
+# Add sliders for adjusting parameters
+axcolor = 'lightgoldenrodyellow'
+ax_a = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+ax_b = plt.axes([0.25, 0.05, 0.65, 0.03], facecolor=axcolor)
+ax_c = plt.axes([0.25, 0.00, 0.65, 0.03], facecolor=axcolor)
+
+s_a = Slider(ax_a, 'a', 0.01, 1.0, valinit=a)
+s_b = Slider(ax_b, 'b', 0.01, 0.1, valinit=b)
+s_c = Slider(ax_c, 'c', 0.01, 1.0, valinit=c)
+
+def update(val):
+    a = s_a.val
+    b = s_b.val
+    c = s_c.val
+    time, completion_rate = simulate_completion_rate(num_steps, dt, a, b, c, noise_type="brownian")
+    line.set_ydata(completion_rate)
+    fig.canvas.draw_idle()
+
+s_a.on_changed(update)
+s_b.on_changed(update)
+s_c.on_changed(update)
+
 plt.show()
